8a5c981d9dc973a3cf661a4b9a214c3ca6b9f218
Grudge69 <shubham.shubham.rawat090@gmail.com>
2023-08-27 23:16:06 +0530

    changes

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-const { MONGODB_URL } = process.env;
+const { MONGODB_URL } = process.env
 
-exports.connect = () => {
-	mongoose
-		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
-		})
-		.then(console.log(`DB Connection Success`))
-		.catch((err) => {
-			console.log(`DB Connection Failed`);
-			console.log(err);
-			process.exit(1);
-		});
-};
+exports.connect = async () => {
+  try {
+    await mongoose.connect(MONGODB_URL, {
+      useNewUrlparser: true,
+      useUnifiedTopology: true,
+    })
+
+    console.log(`DB Connection Success`)
+  } catch (err) {
+    console.log(`DB Connection Failed`)
+    console.log(err)
+    process.exit(1)
+  }
+}
