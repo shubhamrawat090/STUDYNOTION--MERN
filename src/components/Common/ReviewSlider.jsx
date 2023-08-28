de569178f41edde3b06af95108b80947c0c30ca9
Grudge69 <shubham.shubham.rawat090@gmail.com>
2023-08-28 20:12:29 +0530

    review slider css fix

--- src/components/Common/ReviewSlider.jsx
+++ src/components/Common/ReviewSlider.jsx
@@ -53,7 +53,7 @@ function ReviewSlider() {
           {reviews.map((review, i) => {
             return (
               <SwiperSlide key={i}>
-                <div className="flex flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25">
+                <div className="flex min-w-[200px] flex-col gap-3 bg-richblack-800 p-3 text-[14px] text-richblack-25">
                   <div className="flex items-center gap-4">
                     <img
                       src={
